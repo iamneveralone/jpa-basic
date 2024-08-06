ba207fba10c5618dc67e7bebfa11fbd92dbafef6
iamneveralone <fieldsawarded@naver.com>
2024-08-06 22:41:24 +0900

    feat: 회원 저장 (EntityManager 사용)

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); // 사용 다 하면 EntityManager 꼭 닫아주자
+        }
+
+        emf.close();
+    }
+}
